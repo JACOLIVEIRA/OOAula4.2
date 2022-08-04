b32acd1c5578b006bf56bbfee4636c80ef7f594b
JACOLIVEIRA <talento.jacque@gmail.com>
2022-08-04 10:50:22 -0300

    Inicial commit

---
+++ src/RodarAplicacao.java
@@ -0,0 +1,19 @@
+import com.sun.tools.javac.Main;
+
+public class RodarAplicacao {
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+       // Gerente gerente_ = new Funcionario();
+       // Vendedor vendedor_ = (Vendedor) new Funcionario();
+
+
+    }
+
+
+}
